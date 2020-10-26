3b803da0b436b1fa633b65c8422079e07c9589e8
theamanshakya <theamanshakya@gmail.com>
2020-10-26 21:27:46 +0530

    Changes Updated

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const ejs = require("ejs");
 const app = express();
 
 // Static location
-app.use(express.static(__dirname +"/public"));
+app.use(express.static(__dirname + "/public"));
 
 // View-engine
 app.set('view engine', 'ejs');
@@ -17,11 +17,17 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.get("/", function(req, res) {
-    res.render("home");
+app.get("/", function (req, res) {
+  res.render("home");
 });
 
+app.get("/trackbooking", function (req, res) {
+  res.render("trackbooking");
+});
 
+app.get("/login", function (req, res) {
+  res.render("loginpage");
+});
 // Port Listening of Server
 
 // let port = process.env.PORT;
@@ -29,6 +35,6 @@ app.get("/", function(req, res) {
 //   port=3000;
 // };
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server has started sucessfully");
 });
